35ea8a72e7b7c2279edce4b7e9d309b61170f6e7
Volodymyr Horpynchenko <115591313+blowmybow@users.noreply.github.com>
2023-09-14 21:22:58 +0300

    to create repository

---
+++ server.js
@@ -0,0 +1,16 @@
+const app = require("./app");
+const { DB_HOST } = process.env;
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
