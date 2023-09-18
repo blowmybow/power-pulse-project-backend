8081e8918be8778a90297a1fcb1c15840ab60d1b
Volodymyr Horpynchenko <115591313+blowmybow@users.noreply.github.com>
2023-09-18 20:08:52 +0300

    update app, routes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
-const authRouter = require("./routes/api/users");
+const authRouter = require("./routes/api/auth");
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
