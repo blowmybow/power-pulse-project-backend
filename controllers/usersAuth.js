1b50e1d2df2c8ca2b9245cefe71c58d35b8b3c95
Volodymyr Horpynchenko <115591313+blowmybow@users.noreply.github.com>
2023-09-21 13:44:15 +0300

    add cloudinary

--- controllers/usersAuth.js
+++ controllers/usersAuth.js
@@ -1,14 +1,12 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
-const path = require("path");
 const fs = require("fs/promises");
-const Jimp = require("jimp");
 
 const { User } = require("../models/user");
 const { ctrlWrapper, HttpError, dailyCaloriesCalc } = require("../helpers");
+const { cloudinary } = require("../middlewares");
 
-const avatarDir = path.resolve("public", "avatars");
 const { SECRET_KEY } = process.env;
 
 const register = async (req, res) => {
@@ -149,6 +147,7 @@ const updateUsername = async (req, res) => {
     },
   });
 };
+
 const getCurrent = async (req, res) => {
   const { token, name, email, avatarURL } = req.user;
 
@@ -169,34 +168,48 @@ const logout = async (req, res) => {
 };
 
 const updateAvatar = async (req, res) => {
-  if (!req.file) {
-    throw HttpError(400, "Avatar must be provided");
-  }
+  const { email, _id } = req.user;
+  const { path } = req.file;
 
-  const { _id } = req.user;
-  const { path: tempUpload, originalname } = req.file;
-
-  await Jimp.read(tempUpload)
-    .then((avatar) => {
-      return avatar.resize(250, 250).quality(60).write(tempUpload);
-    })
-    .catch((err) => {
-      throw err;
-    });
-
-  const fileName = `${_id}_${originalname}`;
-
-  const resultUpload = path.join(avatarDir, fileName);
-
-  await fs.rename(tempUpload, resultUpload);
-
-  const avatarURL = path.join("avatars", fileName);
-
-  await User.findByIdAndUpdate(_id, { avatarURL });
-
-  res.json({
-    avatarURL,
+  const result = await cloudinary.uploader.upload(path, {
+    folder: "power_pulse_project_avatars",
+    public_id: _id,
   });
+
+  const user = await User.findOneAndUpdate(
+    { email },
+    { avatarUrl: result.url },
+    { new: true }
+  );
+
+  await fs.unlink(path);
+
+  res.status(200).json({
+    user: {
+      name: user.name,
+      avatar: user.avatarUrl,
+    },
+  });
+  // if (!req.file) {
+  //   throw HttpError(400, "Avatar must be provided");
+  // }
+  // const { _id } = req.user;
+  // const { path: tempUpload, originalname } = req.file;
+  // await Jimp.read(tempUpload)
+  //   .then((avatar) => {
+  //     return avatar.resize(250, 250).quality(60).write(tempUpload);
+  //   })
+  //   .catch((err) => {
+  //     throw err;
+  //   });
+  // const fileName = `${_id}_${originalname}`;
+  // const resultUpload = path.join(avatarDir, fileName);
+  // await fs.rename(tempUpload, resultUpload);
+  // const avatarURL = path.join("avatars", fileName);
+  // await User.findByIdAndUpdate(_id, { avatarURL });
+  // res.json({
+  //   avatarURL,
+  // });
 };
 
 module.exports = {
--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
 const authenticate = require("./authenticate");
 const upload = require("./upload");
+const cloudinary = require("./cloudinary");
 
 module.exports = {
   validateBody,
   isValidId,
   authenticate,
   upload,
+  cloudinary,
 };
