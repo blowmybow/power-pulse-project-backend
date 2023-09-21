dec0bad60d72e5ecc2d187cf5546cca6a8ccfa6a
Volodymyr Horpynchenko <115591313+blowmybow@users.noreply.github.com>
2023-09-21 20:16:36 +0300

    update app

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use("/api/auth", authRouter);
+app.use("/auth", authRouter);
 app.use("/exercises", exercisesRouter);
 app.use("/products", productsRouter);
 app.use("/statistics", statisticsRouter);
